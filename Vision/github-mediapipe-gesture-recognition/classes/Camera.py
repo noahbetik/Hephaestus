8a616bc731fa4f468a152788c18a31e59afdcd76
Tyler <tylerjosefmoss@gmail.com>
2024-03-20 11:28:22 -0400

    Initial ML revamp commits and files

---
+++ Vision/github-mediapipe-gesture-recognition/classes/Camera.py
@@ -0,0 +1,8 @@
+class Camera:
+    def __init__(self, settings):
+        # Initialize camera with settings
+        pass
+
+    def capture_frame(self):
+        # Capture and return a single frame
+        return frame
---
+++ Vision/github-mediapipe-gesture-recognition/classes/TCP.py
@@ -0,0 +1,8 @@
+class TCPCommunication:
+    def __init__(self, tcp_settings):
+        # Set up TCP connection with the provided settings
+        pass
+
+    def send(self, command):
+        # Send a command over the TCP connection
+        pass
