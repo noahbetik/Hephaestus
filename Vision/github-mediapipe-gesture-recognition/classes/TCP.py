2419a581e236e5598e47e727e9be0a83187538b1
Noah Betik <noah.betik@gmail.com>
2024-04-08 17:00:15 -0400

    more tcp fixes and sketch removal

--- Vision/github-mediapipe-gesture-recognition/classes/TCP.py
+++ Vision/github-mediapipe-gesture-recognition/classes/TCP.py
@@ -21,47 +21,43 @@ class TCPCommunication:
             print(f"Failed to connect to {self.host}:{self.port}, {e}")
             self.connection = None
 
-    def send_command(self, command):
- 
 
+    def send_command(self, command):
         command += "\n"
         if self.connection:
             try:
-              # Wait for acknowledgment
-                ready = select.select([self.connection], [], [], 0.1)  # Adjusted timeout for demonstration
-                if ready[0]:
-                    data = self.connection.recv(1024).decode("ascii")
-                    #print("received ", data)
-                    command_response = data.split(" ")[0].strip()
-                    
-                    # Default to acknowledgment unless proven otherwise
-                    self.last_command_acknowledged = True
-                    
-                    if "RST" in command_response:    
-                        self.rst = 1
-                    #    print("Received RST packet.")
-                    elif "SEL" in command_response:                       
-                        self.sel = 1
-                        self.desel = 0
-                       # print("Received SEL packet.")
-                    elif "DES" in command_response:    
-                        self.desel = 1
-                        self.sel = 0
-                       # print("Received DES packet.")
-                    else:
-                        # Only in this case did we not receive a known response
-                        self.last_command_acknowledged = False
-                        print(f"Received unexpected response: {data}")
+                # Wait for acknowledgment
+                # ready = select.select([self.connection], [], [], 0.03)  # Adjusted timeout for demonstration
+                # if ready[0]:
+                data = self.connection.recv(1024).decode("ascii")
+                print("received ", data)
+                command_response = data.split(" ")[0].strip()
+                
+                # Default to acknowledgment unless proven otherwise
+                self.last_command_acknowledged = True
+                
+                if "RST" in command_response:    
+                    self.rst = 1
+                #    print("Received RST packet.")
+                elif "SEL" in command_response:                       
+                    self.sel = 1
+                    self.desel = 0
+                    # print("Received SEL packet.")
+                elif "DES" in command_response:    
+                    self.desel = 1
+                    self.sel = 0
+                    # print("Received DES packet.")
                 else:
-                    #print("Acknowledgment not received within timeout.")
+                    # Only in this case did we not receive a known response
                     self.last_command_acknowledged = False
-                        
-                if not self.last_command_acknowledged:
-                    #print("Last command not acknowledged. Not sending new command.")
-                    return
-                else:
+                    print(f"Received unexpected response: {data}")
+                if self.last_command_acknowledged:
                     self.connection.sendall(command.encode("ascii"))
-                    #print(f"Sent: {command.strip()}")
+                    print(f"Sent: {command.strip()}")
+                    
+                else:
+                    print("Last command not acknowledged. Not sending new command.")
+                    return
 
             except Exception as e:
                 print(f"Failed to send data: {e}")
@@ -71,6 +67,8 @@ class TCPCommunication:
         else:
             print("No connection established. Trying to reconnect...")
             self.connect()
+
+
     def close(self):
         if self.connection:
             self.connection.close()
