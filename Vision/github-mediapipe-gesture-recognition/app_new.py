430e96f02dc3d5833cdca7d3e0601757cf3e471f
Tyler Moss <tylerjosefmoss@gmail.com>
2024-04-07 22:25:18 -0400

    initial tcp changes

--- Vision/github-mediapipe-gesture-recognition/app_new.py
+++ Vision/github-mediapipe-gesture-recognition/app_new.py
@@ -6,6 +6,7 @@ import json
 import os
 import socket
 import time
+import select
 import sys
 import cv2 as cv
 import numpy as np
@@ -150,6 +151,7 @@ def main():
     axis = "x"
     view = "home"
     object = "cube"
+    extrude_allowed = False
 
     gesture_types = load_gesture_definitions("./tcp/gestures.json")
 
@@ -160,6 +162,41 @@ def main():
     tcp_communication = TCPCommunication(gesture_processing)
     # state_machine = StateMachine()
     while True:
+        try:
+            # Check for incoming data with no blocking
+            ready = select.select([tcp_communication.connection], [], [], 0)
+
+            if ready[0]:
+                # Data is available to be read
+                data = tcp_communication.connection.recv(1024).decode("ascii")
+                
+                if data.startswith("ACK"):
+                    # Split the data and check the ACK value
+                    print(data)
+                    ack_value = data.split(" ")[1].strip()
+                    print(f"ack_value: {ack_value}")
+                    if ack_value == "0":
+                        extrude_allowed = False
+                        print("Extrude not allowed")
+                    else:
+                        try:
+                            num = int(ack_value)
+                            if num >= 1:
+                                extrude_allowed = True
+                                # print("Extrude allowed")
+                        except ValueError:
+                            print("Invalid ACK number:", ack_value)
+                elif data.strip() == "RST":
+                    state_machine = 3
+                    print("Resetting state machine")
+                    continue  # Proceed with the next iteration of the loop
+                # else:
+                    # print("Unknown or malformed packet:", data)S
+            else:
+                # No data available, non-blocking mode, just pass
+                pass
+        except:
+            print("The first try-except failed.")
         try:
             camera.calculate_fps()  # Calculate camera FPS, stored in camera.fps
 
@@ -290,7 +327,7 @@ def main():
                             f"\rGesture: {gesture_name}, Frames: {frame_counter}, Confidence: {confidence:.5f}"
                         )
                         sys.stdout.flush()
-                        if frame_counter >= frame_threshold:
+                        if (frame_counter >= frame_threshold and  ((extrude_allowed and right_hand_gesture_id == 12) or (right_hand_gesture_id != 12))):
                             frame_counter = 0
                             gesture_type = gesture_types[
                                 gesture_list[right_hand_gesture_id]
@@ -298,7 +335,7 @@ def main():
                             gesture_subtype = gesture_types[
                                 gesture_list[right_hand_gesture_id]
                             ]["subtype"]
-
+                        
                             match left_hand_gesture_id:
                                 case 1:  # 1 finger
                                     axis = "x"
